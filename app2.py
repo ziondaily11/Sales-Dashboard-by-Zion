9e94d435efd69b993f765fdd81e022a654ae7d9a
ziondaily11 <ziondaily11@gmail.com>
2026-06-20 18:45:17 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -47,7 +47,7 @@ def download_csv(pub_url):
 def excel_store():
     orders_dt = download_csv(
         "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6RYGTdLCVG45nqXIeXW0zfH0T3f1OivuPbDa2VhiommrcY2ePm5eWydX-RTmF4ljukwOdmSFl-MLU/pub?output=csv")
-    order_items_dt= download_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRTjMlwObm4-Ia_thTRFUzB8fMKx5LVUmPXWFgeE1r55sbZVamiWqbY-0Beh34g9Ikk8sC0DWXN7w2-/pub?output=csv")
+    order_items_dt= download_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTM0OV17ARTPBIFMnQC-DmYIZMcQ5pvGVuHVuXFcT_lsUG1PNfe5aDj12s9Zva3ebAUWasIGPeTNAUp/pub?output=csv")
     
   
 
