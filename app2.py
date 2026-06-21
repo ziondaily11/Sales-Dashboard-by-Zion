1e0508ab9f3e9c27f22be171699ec3a00b92032f
ziondaily11 <ziondaily11@gmail.com>
2026-06-21 10:08:38 +0300

    making margin for chat

--- app2.py
+++ app2.py
@@ -210,6 +210,7 @@ def show_home():
     cat_chart.update_traces(
       textposition= "outside")
     cat_chart.update_layout(
+        margin= dict(t= 40, b= 10, l= 10, r= 10),
         xaxis= dict(
             showgrid= False,
             title= None
